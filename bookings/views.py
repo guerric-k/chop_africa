85d8eb2ac0f9467d9bac6e4ff1d159b3e79e3bef
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-03-14 03:43:19 +0000

    moved home and menu page to templates in top directory and created register page

--- bookings/views.py
+++ bookings/views.py
@@ -53,79 +53,79 @@ def menu_view(request):
     # Ekwang
     meals.extend([
         {'name': f'Ekwang {i}', 'description': f'A delicious mix of crushed Cocoyams, beef, leaves {i}', 'price': i * 10, 'image': f'images/ekwang{i}.jpeg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Ndole
     meals.extend([
         {'name': f'Ndole {i}', 'description': f'Bitter leaf stew and boiled plantains with beef and Shrimps {i}', 'price': i * 12, 'image': f'images/ndole{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Grilled Mackerel
     meals.extend([
         {'name': f'Grilled Mackerel {i}', 'description': f'Grilled fish with boiled fermented cassava {i}', 'price': i * 15, 'image': f'images/grilled_mackerel{i}.jpeg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Puff Puff
     meals.extend([
         {'name': f'Puff Puff {i}', 'description': f'Fried dough {i}', 'price': i * 5, 'image': f'images/puff_puff{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Suya
     meals.extend([
         {'name': f'Suya {i}', 'description': f'Spicy grilled beef {i}', 'price': i * 8, 'image': f'images/suya{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Akara
     meals.extend([
         {'name': f'Akara {i}', 'description': f'Spicy crushed fried beans {i}', 'price': i * 8, 'image': f'images/akara{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Jollof Rice
     meals.extend([
         {'name': f'Jollof Rice {i}', 'description': f'Spicy rice with Chicken {i}', 'price': i * 30, 'image': f'images/jollof{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Fulere
     meals.extend([
         {'name': f'Fulere {i}', 'description': f'Home made drink {i}', 'price': i * 8, 'image': f'images/fulere{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     # Peanut Soup
     meals.extend([
         {'name': f'Groundnut Soup {i}', 'description': f'Peanut stew with beef chops {i}', 'price': i * 8, 'image': f'images/groundnut_stew{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     #Moin Moin
     meals.extend([
         {'name': f'Moin Moin {i}', 'description': f'Steamed beans pudding {i}', 'price': i * 8, 'image': f'images/moin_moin{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
 
     #Tiga nut drink
     meals.extend([
         {'name': f'Tiga Nut Drink {i}', 'description': f'Juiced Tiga nuts with ice blocks and Dates {i}', 'price': i * 8, 'image': f'images/tiger_nut{i}.jpg'}
-        for i in range(1, 21)
+        for i in range(1, 12)
     ])
     
     #Chapman Drink
     meals.extend([
-        {'name': f'Chapman Drink {i}', 'description': f'Home made drinks {i}',
-         'price': i * 8, 'image': f'images/chapman{i}.jpg'} for i in range(1, 21)
+        {'name': f'Chapman Drink {i}', 'description': f'Home made drinks {i}', 'price': i * 8, 'image': f'images/chapman{i}.jpg'} 
+        for i in range(1, 12)
     ])
 
     paginator = Paginator(meals, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'bookings/menu.html', {'meals': page_obj})
+    return render(request, 'menu.html', {'meals': page_obj})
 
 # Contact view
 def contact_view(request):
@@ -140,7 +140,7 @@ def home_view(request):
         {'name': 'Puff Puff', 'description': 'Fried dough', 'image': 'images/puff_puff.jpg'},
         {'name': 'suya', 'Grilled Beef': 'Spicy grilled beef', 'image': 'images/suya.jpg'},
     ]
-    return render(request, 'bookings/home.html', {'featured_meals': featured_meals})
+    return render(request, 'home.html', {'featured_meals': featured_meals})
 
 # Reservation view
 def make_reservation(request):
@@ -163,7 +163,7 @@ def make_reservation(request):
                 messages.error(request, str(e))
     else:
         form = ReservationForm()
-    return render(request, 'bookings/make_reservation.html', {'form': form})
+    return render(request, 'make_reservation.html', {'form': form})
 
 # View reservations view
 def view_reservations(request):
@@ -173,7 +173,7 @@ def view_reservations(request):
 
     user = User.objects.get(customer_id=request.session['user_id'])
     reservations = Reservation.objects.filter(user=user)
-    return render(request, 'bookings/view_reservations.html', {'reservations': reservations})
+    return render(request, 'view_reservations.html', {'reservations': reservations})
 
 # Modify reservation view
 def modify_reservation(request, reservation_id):
@@ -191,7 +191,7 @@ def modify_reservation(request, reservation_id):
     else:
         form = ReservationForm(instance=reservation)
 
-    return render(request, 'bookings/modify_reservation.html', {'form': form, 'reservation': reservation})
+    return render(request, 'modify_reservation.html', {'form': form, 'reservation': reservation})
 
 # Delete reservation view
 def delete_reservation(request, reservation_id):
@@ -202,5 +202,5 @@ def delete_reservation(request, reservation_id):
         messages.success(request, 'Reservation deleted successfully.')
         return redirect('view_reservations')
 
-    return render(request, 'bookings/delete_reservation_confirm.html', {'reservation': reservation})
+    return render(request, 'delete_reservation_confirm.html', {'reservation': reservation})
 
